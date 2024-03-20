252df748fe6bdbcfc4fa2b3aea7ac0a323327743
keisuke <ronkun0658@yahoo.co.jp>
2024-03-20 16:19:19 +0900

    mapcomp3

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
 const viewer = new Cesium.Viewer("cesiumContainer", {
-    baseLayer: Cesium.ImageryLayer.fromProviderAsync(
-      Cesium.TileMapServiceImageryProvider.fromUrl(
-        Cesium.buildModuleUrl("../Assets/Textures/NaturalEarthII")
-      )
-    ),
-    baseLayerPicker: false,
-    geocoder: false,
-  });
-  
-const scene = viewer.scene;
-//viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
-
-
-  
-
-viewer.camera.flyTo({
-  destination: Cesium.Cartesian3.fromDegrees(135, 35, 5000000.0),
+  baseLayer: Cesium.ImageryLayer.fromProviderAsync(
+    Cesium.TileMapServiceImageryProvider.fromUrl(
+      Cesium.buildModuleUrl("/Build/CesiumUnminified/Assets/Textures/NaturalEarthII")
+    )
+  ),
+  baseLayerPicker: false,
+  geocoder: false,
 });
 
+const scene = viewer.scene;
+//viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
 viewer.entities.add({
   id: "Tokyo",
-  position: Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+  position: Cesium.Cartesian3.fromDegrees(135, 35,500),
   point: {
     color: Cesium.Color.RED,
     pixelSize: 8,
   },
 });
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(135, 35, 5000000.0),
+});
+
 
