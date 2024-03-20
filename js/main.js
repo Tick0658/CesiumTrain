2a578cddbb1b57f4a4efea86375f6140289b12bb
keisuke <ronkun0658@yahoo.co.jp>
2024-03-20 23:36:54 +0900

    release

--- js/main.js
+++ js/main.js
@@ -14,10 +14,8 @@ function Init() {
 
 viewer = Init();
 
-
-
 const scene = viewer.scene;
-//viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+
 viewer.entities.add({
   id: "Tokyo",
   position: Cesium.Cartesian3.fromDegrees(136.881637, 35.170694),
