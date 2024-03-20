603c9b0beca44b4bbc0fb55745c558b206d192ee
keisuke <ronkun0658@yahoo.co.jp>
2024-03-20 21:41:31 +0900

    ALL Comp

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-const viewer = new Cesium.Viewer("cesiumContainer", {
-  baseLayer: Cesium.ImageryLayer.fromProviderAsync(
-    Cesium.TileMapServiceImageryProvider.fromUrl(
-      Cesium.buildModuleUrl("/Build/Cesium/Assets/Textures/NaturalEarthII")
-    )
-  ),
-  //baseLayerPicker: false,
-  geocoder: false,
-});
+function Init() {
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+    baseLayer: Cesium.ImageryLayer.fromProviderAsync(
+      Cesium.TileMapServiceImageryProvider.fromUrl(
+        Cesium.buildModuleUrl("/Build/Cesium/Assets/Textures/NaturalEarthII")
+      )
+    ),
+    //baseLayerPicker: false,
+    geocoder: false,
+  });
+
+  return viewer;
+}
+
+viewer = Init();
+
+
 
 const scene = viewer.scene;
 //viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
 viewer.entities.add({
   id: "Tokyo",
-  position: Cesium.Cartesian3.fromDegrees(136.881637,35.170694),
+  position: Cesium.Cartesian3.fromDegrees(136.881637, 35.170694),
   point: {
     color: Cesium.Color.RED,
     pixelSize: 8,
@@ -22,4 +30,21 @@ viewer.camera.flyTo({
   destination: Cesium.Cartesian3.fromDegrees(135, 35, 5000000.0),
 });
 
+var _target = "";
 
+fetch("../CesiumCzml.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    
+    _target = JSON.parse(data);
+  })
+  .catch((error) => {
+    console.error("エラー:", error);
+  });
+
+setTimeout(() => {
+  console.log(_target);
+  const dataSourcePromise = viewer.dataSources.add(
+    Cesium.CzmlDataSource.load(_target)
+  );
+}, 10000);
